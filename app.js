eca43bd2b98bcf651c7a5faea9795b81176f4cf4
Ali Rawashdeh <ali@roses.local>
2016-08-22 23:36:27 +0100

    updating with latest code from github

--- app.js
+++ app.js
@@ -31,10 +31,88 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'))
    .use(cookieParser());
 
-app.get('/authredirect', function(req, res) {
+app.get('/login', function(req, res) {
 
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  res.redirect('https://www.wunderlist.com/oauth/authorize?' +
+    querystring.stringify({
+      client_id: client_id,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
 });
 
+app.get('/callback', function(req, res) {
+
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    res.clearCookie(stateKey);
+    var authOptions = {
+      url: 'https://www.wunderlist.com/oauth/access_token',
+      form: {
+        code: code,
+        client_id: client_id,
+        client_secret: client_secret,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        var access_token = body.access_token,
+            refresh_token = body.refresh_token;
+
+        var options = {
+          url: 'https://a.wunderlist.com/v1/me',
+          headers: {
+          'X-Access-Token': + access_token,
+          'X-Client-ID': authOptions.form.client_id
+        },
+          json: true
+        };
+
+        // use the access token to access the Wunderlist Web API
+        request.get(options, function(error, response, body) {
+          console.log(body);
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect('/#' +
+          querystring.stringify({
+            access_token: access_token,
+            client_id: authOptions.form.client_id,
+            refresh_token: refresh_token
+          }));
+
+
+      } else {
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
+  }
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
